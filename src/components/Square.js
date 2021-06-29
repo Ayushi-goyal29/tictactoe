64fe5feddcc66df0a896492aea29b79727fcea95
Ayushi-goyal29 <71444685+Ayushi-goyal29@users.noreply.github.com>
2021-06-29 13:54:42 +0530

    Add files via upload

---
+++ src/components/Square.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Square = ({ value, onClick, isWinningSquare }) => {
+  return (
+    <button
+      type="button"
+      className="square"
+      onClick={onClick}
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
+    >
+      {value}
+    </button>
+  );
+};
+
+export default Square;
